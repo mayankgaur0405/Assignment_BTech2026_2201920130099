2d9834c29f39be3fe61c43d3953c2e4213371ed4
Mayank Gaur <mayankgaur1504@gmail.com>
2025-01-29 09:01:05 +0530

    28-jan-2025 assignment completed

---
+++ [28-01-2025]/OOPS/Encapsulation and Accessors Usage.cpp
@@ -0,0 +1,25 @@
+// #include <iostream>
+// using namespace std;
+
+// class User {
+// private:
+//     string name = "Alice";
+//     string password = "Wonderland";
+
+// public:
+//     void set(string name, string password) {
+//         this->name = name;
+//         this->password = password;
+//     }
+//     void get() {
+//         cout << name << " " << password << endl;
+//     }
+// };
+
+// int main() {
+//     User obj;
+//     obj.get(); // Print the initial name and password
+//     obj.set("Tom", "Jerry"); // Change the name and password
+//     obj.get(); // Print the updated name and password
+//     return 0;
+// }
