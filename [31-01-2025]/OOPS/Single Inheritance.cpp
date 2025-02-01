2ac0b85295a862a1ba08855a214fcf9120aca12b
Mayank Gaur <mayankgaur1504@gmail.com>
2025-02-01 08:13:31 +0530

    [31-01-2025] assignment completed

---
+++ [31-01-2025]/OOPS/Single Inheritance.cpp
@@ -0,0 +1,40 @@
+// #include <iostream>
+// using namespace std;
+
+// // Base class
+// class Animal {
+// public:
+//     string name;
+
+//     Animal(string name) {
+//         this->name = name;
+//     }
+
+//     void Eat() {
+//         cout << name << " is eating." << endl;
+//     }
+
+//     void sleep() {
+//         cout << name << " is sleeping." << endl;
+//     }
+// };
+
+// // Derived class
+// class Dog : public Animal {
+// public:
+//     Dog(string name) : Animal(name) {}
+
+//     void Bark() {
+//         cout << "Woof! Woof!" << endl;
+//     }
+// };
+
+// int main() {
+//     Dog myDog("Buddy");
+
+//     myDog.Eat();
+//     myDog.sleep();
+//     myDog.Bark();
+
+//     return 0;
+// }
