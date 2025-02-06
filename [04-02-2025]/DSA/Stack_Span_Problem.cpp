fff62aa89b1cb35d9440b930a430f5b525236ef9
Mayank Gaur <mayankgaur1504@gmail.com>
2025-02-06 20:26:11 +0530

    04-03-2025 date updated to 04-02-2025

---
+++ [04-02-2025]/DSA/Stack_Span_Problem.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> price = {100 , 80 , 60 , 70 , 60 , 75 , 85};
+   
+
+    //solution
+    vector<int> ans(price.size() , 0);
+     stack<int> s;
+
+     for(int i = 0 ; i < price.size() ; i++){
+        while(s.size() > 0 && price[s.top()] <= price[i]){
+            s.pop();
+        }
+
+        if(s.empty()){
+            ans[i] = i+1;
+        }else{
+            ans[i] = i - s.top();
+            
+        }
+
+        s.push(i);
+      
+     }
+
+     for(int val : ans){
+        cout<<val<<" ";
+     }
+    return 0;
+}
