fc5878419976e4f00eee60cd8a62e39d04f32c37
Mayank Gaur <mayankgaur1504@gmail.com>
2025-02-06 20:27:10 +0530

    05-02-2025 assignment completed

---
+++ [05-02-2025]/OOPS/encapsulation.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Encapsulation : It is the wrapping up to data members and member functions
+//It is used for data hiding
+class Account{
+private:
+string password;
+double salary;
+
+public:
+string userName;
+string bankName;
+};
+int main(){
+    return 0;
+}
